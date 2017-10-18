ffb41a86a6b42b6b3e1acd1809395fa465b1766c
Syu125 <yusophia2011@gmail.com>
2017-10-18 02:10:20 -0700

    the riddler secret message box

--- src/SecretMessageBox.java
+++ src/SecretMessageBox.java
@@ -16,17 +16,19 @@ import javax.swing.JOptionPane;
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
+	public static void main(String[] args) {
+		
 	
 	// 1. Set the passcode in a String variable
-	
+	String panther="Huh";
 	// 2. Using a pop-up, ask for a secret message and store it in a variable
-
+String rehtnap=JOptionPane.showInputDialog("cool");
 	// 3. Ask your friend for the passcode and store it in a variable
-
+String Isaac=JOptionPane.showInputDialog("think");
 	// 4. If the passcode matches, show the secret message
 
 	// 5. If the passcode does not match, pop-up "INTRUDER!!"
 
 	// [optional] 6. Have your friend also enter a username, and make sure it is correct before releasing the secret message.
-
+	}
 }
--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -8,17 +10,22 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int leyva=(0);
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String andres=JOptionPane.showInputDialog("There where 2 animals and 2 people.The 2 people went to go get some food when they came back  they saw the 2 animals in blood.What happend to the 2 animals. ");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if(andres.equals("the animals where fighting and they where pittbulls")){
+JOptionPane.showMessageDialog(null, "correct");
+leyva=leyva+1;
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+else{
+	JOptionPane.showMessageDialog(null, "wrong");
+}
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
-		
+		JOptionPane.showMessageDialog(null,leyva);
 	}
 }
 
